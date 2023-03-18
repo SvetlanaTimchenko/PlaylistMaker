ac0ec2f09f3b327ba808d328827748cbf89fb901
SvetlanaTimchenko <svetlana010785@yandex.ru>
2023-03-18 14:33:42 +0300

    добавлено поле поиска и кнопки очистки поиска, домашка по теме EditText и TextWatcher

--- app/src/main/java/com/timchenko/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/timchenko/playlistmaker/SearchActivity.kt
@@ -3,6 +3,10 @@ package com.timchenko.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
@@ -15,5 +19,28 @@ class SearchActivity : AppCompatActivity() {
         buttonBack.setOnClickListener {
             this.finish()
         }
+
+        val inputEditText = findViewById<EditText>(R.id.searchEditText)
+        val clearSearchButton = findViewById<ImageView>(R.id.searchClear)
+
+        clearSearchButton.setOnClickListener {
+            inputEditText.setText("")
+            clearSearchButton.visibility = View.GONE
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.isNullOrEmpty()) {
+                    clearSearchButton.visibility = View.VISIBLE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
     }
 }

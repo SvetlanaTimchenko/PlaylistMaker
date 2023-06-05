b4028e127751a2b63aa6d79460c4a6a9e4113718
SvetlanaTimchenko <svetlana010785@yandex.ru>
2023-06-05 22:52:23 +0300

    спринт 14: аудиопреер с семплом 30 сек

--- app/src/main/java/com/timchenko/playlistmaker/AudioPlayerActivity.kt
+++ app/src/main/java/com/timchenko/playlistmaker/AudioPlayerActivity.kt
@@ -1,8 +1,12 @@
 package com.timchenko.playlistmaker
 
+import android.media.MediaPlayer
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -29,6 +33,23 @@ class AudioPlayerActivity : AppCompatActivity() {
     private lateinit var albumGroup : Group
     private lateinit var album : TextView
 
+    private lateinit var play: ImageButton
+    private lateinit var timeBar: TextView
+    private var mediaPlayer = MediaPlayer()
+    private var playerState = STATE_DEFAULT
+    private var playIconId: Int = 0
+    private val handler = Handler(Looper.getMainLooper())
+    private lateinit var playerRunnable: Runnable
+
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+
+        private const val DELAY = 1000L
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_audio_player)
@@ -37,6 +58,14 @@ class AudioPlayerActivity : AppCompatActivity() {
         val buttonBack = findViewById<ImageView>(R.id.back)
         buttonBack.setOnClickListener {
             this.finish()
+        }
+
+        // проигрыватель
+        play = findViewById(R.id.playBtn)
+        timeBar = findViewById(R.id.timeBar)
+        timeBar.text = "00:30" // устанавливаем таймер для всех отрывков в 30 секунд
+        play.setOnClickListener {
+            playbackControl()
         }
 
         // достаем трек
@@ -79,6 +108,90 @@ class AudioPlayerActivity : AppCompatActivity() {
             .transform(RoundedCorners(resources.getDimensionPixelOffset(R.dimen.value_8)))
             .into(this.findViewById(R.id.trackCover))
 
+        // медиаплеер
+        mediaPlayer.setDataSource(track.previewUrl)
+        preparePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
+    private fun preparePlayer() {
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            playerState = STATE_PREPARED
+        }
+    }
+
+    private fun playbackControl() {
+        when(playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+            }
+        }
+    }
+
+    private fun startPlayer() {
+        val secondsCount = timeBar.text
+            .toString()
+            .replace(":", "")
+            .toLong()
+
+        if (secondsCount > 0) {
+            mediaPlayer.start()
+            playIconId = resources.getIdentifier("buttonpause", "drawable", packageName)
+            play.setImageResource(playIconId)
+            playerState = STATE_PLAYING
+            startTimer(secondsCount)
+        }
+    }
+
+    private fun startTimer(duration: Long) {
+        val startTime = System.currentTimeMillis()
+        playerRunnable = createUpdateTimerTask(startTime, duration * DELAY)
+        handler.post(playerRunnable)
+    }
+
+    private fun createUpdateTimerTask(startTime: Long, duration: Long): Runnable {
+        return object : Runnable {
+            override fun run() {
+                // Сколько прошло времени с момента запуска таймера
+                val elapsedTime = System.currentTimeMillis() - startTime
+                // Сколько осталось до конца
+                val remainingTime = duration - elapsedTime
+
+                if (remainingTime > 0) {
+                    // Если всё ещё отсчитываем секунды —
+                    // обновляем UI и снова планируем задачу
+                    val seconds = remainingTime / DELAY
+                    timeBar.text = String.format("%02d:%02d", seconds / 60, seconds % 60)
+                    handler.postDelayed(this, DELAY)
+                } else {
+                    pausePlayer()
+                }
+            }
+        }
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        playIconId = resources.getIdentifier("buttonplay", "drawable", packageName)
+        play.setImageResource(playIconId)
+        playerState = STATE_PAUSED
+        handler.removeCallbacks(playerRunnable)
     }
 
     private fun <T : Serializable?> getSerializable(name: String, clazz: Class<T>): T

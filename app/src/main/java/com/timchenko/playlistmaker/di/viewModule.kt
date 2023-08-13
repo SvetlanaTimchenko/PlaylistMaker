878980c0e59a4fbe908f425dc093aa05f9257794
SvetlanaTimchenko <svetlana010785@yandex.ru>
2023-08-13 19:40:04 +0300

    спринт 19: Search, Medial, Settings переведены на Fragments. Добавлена BottomNavigationView

--- app/src/main/java/com/timchenko/playlistmaker/di/viewModule.kt
+++ app/src/main/java/com/timchenko/playlistmaker/di/viewModule.kt
@@ -1,9 +1,9 @@
 package com.timchenko.playlistmaker.di
 
 import com.timchenko.playlistmaker.presentation.audioplayer.AudioPlayerViewModel
-import com.timchenko.playlistmaker.presentation.main.MainViewModel
 import com.timchenko.playlistmaker.presentation.media.FavoritesFragmentViewModel
 import com.timchenko.playlistmaker.presentation.media.PlaylistsFragmentViewModel
+import com.timchenko.playlistmaker.presentation.root.RootViewModel
 import com.timchenko.playlistmaker.presentation.search.SearchViewModel
 import com.timchenko.playlistmaker.presentation.settings.SettingsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -23,7 +23,7 @@ val viewModelModule = module {
     }
 
     viewModel {
-        MainViewModel(get())
+        RootViewModel(get())
     }
 
     viewModel {
--- app/src/main/java/com/timchenko/playlistmaker/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/timchenko/playlistmaker/ui/settings/SettingsFragment.kt
@@ -1,28 +1,27 @@
 package com.timchenko.playlistmaker.ui.settings
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.content.res.Configuration
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.timchenko.playlistmaker.R
-import com.timchenko.playlistmaker.databinding.ActivitySettingsBinding
+import com.timchenko.playlistmaker.databinding.FragmentSettingsBinding
 import com.timchenko.playlistmaker.presentation.settings.SettingsViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsFragment : Fragment() {
 
-    private lateinit var binding: ActivitySettingsBinding
+    private lateinit var binding: FragmentSettingsBinding
     private val viewModel: SettingsViewModel by viewModel()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivitySettingsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        // реализация клика на кнопку Назад
-        binding.back.setOnClickListener {
-            this.finish()
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         // переключение темы
         val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
@@ -48,7 +47,6 @@ class SettingsActivity : AppCompatActivity() {
                 text = getString(R.string.contact_message)
             )
         }
-
 
         // кнопка "Пользовательское соглашение"
         binding.buttonEula.setOnClickListener {
